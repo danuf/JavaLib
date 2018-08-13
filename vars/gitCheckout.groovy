01a1f177d9b959f899b35ed9dad673463ac67012
Maksym <Maksym_Husak@epam.com>
2018-08-13 13:40:42 +0000

    Olen

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,1 +1,5 @@
-def call
+def call (Map pipelineParams) {
+    node {
+        checkout([$class: 'GitSCM', branches: [[name: pipelineParams.BranchName]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: pipelineParams.CredentialsId , url: pipelineParams.GitRepository ]]])
+    }
+}
