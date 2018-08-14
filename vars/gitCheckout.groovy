de3145e1b95532426cdd10ffff4bd30d761a8ea1
danuf <33052960+danuf@users.noreply.github.com>
2018-08-14 14:04:59 +0300

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,3 +1,5 @@
+// describe parameters here
+
 def call (Closure body) {
     stage ('GitCheckout') {
         def config = [:]
@@ -5,11 +7,14 @@ def call (Closure body) {
         body.delegate = config
         body()
         checkout([$class: 'GitSCM',
-        branches: [[name: "${config.BranchName}"]],
-        doGenerateSubmoduleConfigurations: config.SubmoduleConfig,
-        extensions: [],
-        submoduleCfg: [],
-        userRemoteConfigs: [[credentialsId: "${config.CredentialsID}" , url: "${config.GitRepository}" ]]
-    ])
+            branches: [[name: "${config.BranchName}"]],
+            doGenerateSubmoduleConfigurations: config.SubmoduleConfig,
+            extensions: [],
+            submoduleCfg: [],
+            userRemoteConfigs: [[
+                credentialsId: "${config.CredentialsID}" , 
+                url: "${config.GitRepository}" 
+            ]]
+        ])
     }
 }
