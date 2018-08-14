214c363a0de6ea4f041c24a00149590225b5af33
danuf <33052960+danuf@users.noreply.github.com>
2018-08-14 16:25:36 +0300

    Update DockerCheckStage.groovy

--- vars/DockerCheckStage.groovy
+++ vars/DockerCheckStage.groovy
@@ -1,11 +1,27 @@
+import com.lab.build.Colorizer
+
+/**
+ * stageCreateVM
+ *
+ * Check docker availability
+ *
+ * @param ApplicationIP
+ * @param TimeOutCheck
+ *
+ * @version $Id: $
+ * @copyright 2018, EPAM, Inc.  All Rights Reserved
+ */
+
 def call (Closure body) {
     stage('Docker Check Stage') {
         def config = [:]
         body.resolveStrategy = Closure.DELEGATE_FIRST
         body.delegate = config
         body()
+	    
         def Response
         timeout (time: config.TimeOutCheck, unit:'SECONDS') { 
+	    // change it with 
             echo 'Check Successful docker container Up'
             sleep 10
             while(Response!="HTTP/1.1 200") {
@@ -13,7 +29,9 @@ def call (Closure body) {
                 Response = Curl[0..11]
 		println Response
             }
-	return Response    
+	return Response
+// Please return human readable information about success or failed response in colorized info echo
+// like this:	echo Colorizer.info('Some text')
         }
     }
 }
