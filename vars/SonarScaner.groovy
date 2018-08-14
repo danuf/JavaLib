849445f9f38c5970a7534117099c710aff962399
Maksym <Maksym_Husak@epam.com>
2018-08-14 10:38:08 +0000

    test

---
+++ vars/SonarScaner.groovy
@@ -0,0 +1,13 @@
+def call (Closure body) {
+    stage ('SonarQube Scan') {
+        def config [:]
+        body.resolveStrategy = Closure.DELEGATE_FIRST
+        body.delegate = config
+        body()
+    
+        withSonarQubeEnv("${config.SonarName}") {
+            sh "${config.SonarHome}/bin/sonar-scanner ${config.SonarKey} ${config.SonarProj} ${config.Sonarbinaries} ${config.SonarSource}"
+        }
+
+    }
+}
