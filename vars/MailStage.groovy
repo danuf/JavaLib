71a702205286acc31e1ea54eaf193f5dae4f56a9
Maksym <Maksym_Husak@epam.com>
2018-08-14 14:22:57 +0000

    Olen

--- vars/MailStage.groovy
+++ vars/MailStage.groovy
@@ -4,7 +4,7 @@ def call(Closure body) {
         body.resolveStrategy = Closure.DELEGATE_FIRST
         body.delegate = config
         body()
-        if(response.equals("HTTP/1.1 200")) {
+        if(config.Check.equals("HTTP/1.1 200")) {
             emailext(subject: "${env.JOB_NAME} was " + "${BUILD_STATUS}", body: "Commit short hash " + "${shortCommit}", to: "${config.Recipient}", replyTo: '');
         }else {
             System.exit(1)
