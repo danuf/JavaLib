7ceb3178a2726e724eceabd2b14e6f773a9ab642
Sanwel <42137467+Sanwel@users.noreply.github.com>
2018-08-14 10:43:32 +0300

    Update and rename mavenBuild.groovy to stageMavenExec.groovy

---
+++ vars/stageMavenExec.groovy
@@ -0,0 +1,31 @@
+import com.lab.build.Colorizer
+
+/**
+ * stageMavenExec
+ *
+ * Execute Maven commands (build, upload, etc) with custom parameters
+ *
+ * @param jdkVersion
+ * @param mavenVersion
+ * @param jvmOptions
+ * @param mavenCommand
+ *
+ * @version $Id: $
+ * @copyright 2018, EPAM systems, All Rights Reserved
+ */
+def call(Closure body) {
+    stage ('Execute Maven') {
+        def config = [:]
+        body.resolveStrategy = Closure.DELEGATE_FIRST
+        body.delegate = config
+        body()
+
+        withMaven(
+            jdk: "${config.jdkVersion}",
+            maven: "${config.mavenVersion}",
+            mavenOpts: "${config.jvmOptions}"
+        ){
+            sh "${config.mavenCommand}"
+        }
+    }
+}
