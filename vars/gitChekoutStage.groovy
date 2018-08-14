5020e9263d857b9201b819c0c8788f9350e188c7
Maksym <Maksym_Husak@epam.com>
2018-08-14 09:27:42 +0000

    test

---
+++ vars/gitChekoutStage.groovy
@@ -0,0 +1,14 @@
+def call (Closure body) {
+    stage ('GitCheckout') {
+        def config = [:]
+        body.resolveStrategy = Closure.DELEGATE_FIRST
+        body.delegate = config
+        body()
+    }
+}
+    checkout([$class: 'GitSCM',
+        branches: [[name: "${config.BranchName}"]],
+        doGenerateSubmoduleConfigurations: "${config.SubmoduleConfig}",
+        extensions: [],
+        submoduleCfg: [],
+        userRemoteConfigs: [[credentialsId: "${config.CredentialsID}" , url: "${config.GitRepository}" ]]])
